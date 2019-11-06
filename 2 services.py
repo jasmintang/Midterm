d56900e3300fd89204332cefd3fba417a0a659bf
jasmintang <44978515+jasmintang@users.noreply.github.com>
2019-11-06 14:47:40 -0500

    Update 2 services.py

--- 2 services.py
+++ 2 services.py
@@ -1,6 +1,8 @@
 import os
 import sys
 from importlib import reload
+from typing import List, Any
+
 from textblob import TextBlob
 import pandas as pd
 from collections import Counter
@@ -15,43 +17,53 @@ blob = TextBlob(fcs)
 np = blob.noun_phrases
 
 
-f_cs = []
-for i in np:
-    i = ''.join(i)
-    blob = TextBlob(i)
-    f_cs.append([blob.sentiment.polarity, i])
-print(f_cs)
+def text(x):
+    f_cs = []
+    for i in x:
+        i = ''.join(i)
+        blob = TextBlob(i)
+        f_cs.append([blob.sentiment.polarity, i])
+    return f_cs
+f_cs = pd.DataFrame(text(np))
+f_cs = f_cs.sort_values(by=0, ascending=False)
 
-f_cs = pd.DataFrame(f_cs)
-f_csr = f_cs.sort_values(by=0, ascending=False)
+# services
+# positive characters
+def positive(f_cs):
+    key = []
+    key2 = []
+    __re_data = dict()
+    for (i, b) in zip(f_cs[0], f_cs[1]):
+        __re_data[b] = i
+    for i in __re_data:
+        key.append(i)
+    for i in range(len(key)):
+        if i > 9:
+            break
+        key2.append(key[i])
+    return key2
+
+# negative characters
+def negative(f_cs):
+    __re_data = dict()
+    for (i, b) in zip(f_cs[0], f_cs[1]):
+        __re_data[b] = i
+    key_1 = []
+    key_r = []
+    for i in __re_data:
+        key_1.append(i)
+    for i in range(len(key_1)):
+        if i > 9:
+            break
+        key_r.append(key_1[len(key_1)-i-1])
+    return key_r
 
 
-__they_data = dict()
-for (i, j) in zip(f_csr[0], f_csr[1]):
-    __they_data[j] = i
-key=[]
-for i in __they_data:
-    key.append(i)
-print("top_10_positive:")
-for i in range(len(key)):
-    if i > 9:
-        break
-    print(key[i])
-
-
-print("\n\ntop_10_negative:")
-for i in range(len(key)):
-    if i > 9:
-        break
-    print(key[len(key)-i-1])
-
-
-__my_data = []
-for i in f_csr[1]:
-    __my_data.append(i)
-my_data = Counter(__my_data)
-
-top_ten = my_data.most_common(10)
-for i in top_ten:
-    print(i)
-print(top_ten)
+# top 10 common words
+def common(f_cs):
+    __my_data = []
+    for i in f_cs[1]:
+        __my_data.append(i)
+        my_data = Counter(__my_data)
+        top_ten = my_data.most_common(10)
+    return top_ten
